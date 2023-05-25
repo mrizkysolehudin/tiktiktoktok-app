3c96ba5af2ee6dfb8a7a8b49d6ee8ca50065d98f
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-05-25 07:03:28 +0700

    finish fitur color button

--- components/PopularTopics.jsx
+++ components/PopularTopics.jsx
@@ -1,8 +1,10 @@
 import { topics } from "@/utils/constants";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const PopularTopics = () => {
+	const [currentTopic, setCurrentTopic] = useState("");
+
 	return (
 		<div className="my-2  border-b-2 border-gray-200 pb-6 pt-2">
 			<h3 className="ml-2 font-semibold text-gray-600">Popular Topics</h3>
@@ -10,9 +12,14 @@ const PopularTopics = () => {
 			<div className="mt-4 flex flex-wrap gap-3">
 				{topics.map((topic, index) => (
 					<Link
-						href={`/?topic=${topic.name}`}
 						key={index}
-						className="flex items-center gap-x-3 rounded-full border-2 border-gray-300 px-3 py-1 font-semibold capitalize hover:bg-gray-100">
+						href={`/?topic=${topic.name}`}
+						onClick={() => setCurrentTopic(topic?.name)}
+						className={`${
+							currentTopic == topic.name
+								? "border-pink-600 text-pink-600"
+								: "border-gray-300"
+						} flex items-center gap-x-3 rounded-full border-2  px-3 py-1 font-semibold capitalize hover:bg-gray-100`}>
 						<p className="text-2xl">{topic.icon}</p>
 
 						<p>{topic.name}</p>
